831f1b51fcacbca41fe2069a559cfc319e437e79
Ben Ifie <jaminonline@gmail.com>
2018-07-23 23:10:07 +0100

    check tests

--- src/reporter/reporter.js
+++ src/reporter/reporter.js
@@ -8,22 +8,37 @@ class SummaryReporter extends events.EventEmitter {
         this.baseReporter = baseReporter;
         this.config = config;
         this.options = options;
+        this.specs = {};
+        this.results = {};
 
         this.on('runner:start', function (runner) {
-            console.log('Runner:', JSON.stringify(runner));
+            this.specs[runner.cid] = runner.specs
+            this.results[runner.cid] = {
+                passing: 0,
+                pending: 0,
+                failing: 0
+            };
         });
 
         this.on('suite:start', function (suite) { });
 
         this.on('test:pending', function (test) {
-            console.log('Cid is', test.cid);
+            this.results[test.cid].pending++;
         });
 
         this.on('test:pass', function (test) {
-            console.log(test);
+            this.results[test.cid].passing++
+        });
+
+        this.on('test:fail', function (test) {
+            this.results[test.cid].failing++
         });
 
         this.on('runner:end', function (runner) {
+        });
+        
+        this.on('end', function (runner) {
+            console.log(JSON.stringify(this.results));
         });
 
     }
